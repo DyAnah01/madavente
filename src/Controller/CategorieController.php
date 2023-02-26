27cc48e7a0f5707d95461e1d370ce7d572a410ef
Diana Raharinosy <dianaraharinosy@yahoo.com>
2023-02-26 20:30:23 +0100

    add and show categorie is done

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use App\Form\CategorieType;
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategorieController extends AbstractController
 {
     #[Route('admin/categorie', name: 'app_categorie')]
-    public function index(): Response
+    public function index(CategorieRepository $repoCat, Request $request, EntityManagerInterface $manager): Response
     {
+        $categorie = $repoCat->findAll();
+
+        $cat = new Categorie;
+        $form = $this->createForm(CategorieType::class, $cat);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($cat);
+            $manager->flush();
+            $this->addFlash('success', 'La catégorie n° '.$cat->getId().'a bien été ajoutée');
+            return $this->redirectToRoute('app_categorie');
+        }
+
         return $this->render('categorie/index.html.twig', [
-            'controller_name' => 'CategorieController',
+            'categorie'=> $categorie,
+            'formCat' => $form->createView(),
         ]);
     }
 }
