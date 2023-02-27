43e301fa01a457fd4ada62fdd2a05bdd8d20db8f
Diana Raharinosy <dianaraharinosy@yahoo.com>
2023-02-27 13:26:16 +0100

    add form ArticlesTypes

---
+++ src/Form/ArticlesType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Articles;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticlesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('photo')
+            ->add('prix')
+            ->add('dateCreation')
+            ->add('stock')
+            ->add('shortDescription')
+            ->add('idCategorie')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Articles::class,
+        ]);
+    }
+}
