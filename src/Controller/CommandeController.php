ff99b40931bf492e5a40e213844d73f5f5f64599
Diana Raharinosy <dianaraharinosy@yahoo.com>
2023-05-04 14:56:17 +0200

    clean commande controller

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -2,20 +2,14 @@
 
 namespace App\Controller;
 
-use App\Entity\Commande;
-use App\Repository\ArticlesRepository;
 use App\Repository\CommandeDetailsRepository;
 use App\Repository\CommandeRepository;
-use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Mapping\Id;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Bundle\SecurityBundle\SecurityBundle;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Security;
 
 class CommandeController extends AbstractController
 {
@@ -45,9 +39,8 @@ class CommandeController extends AbstractController
     }
 
     #[Route('/admin/historiqueCommande', name: 'historique_commande_admin')]
-    public function historique(CommandeDetailsRepository $repoComD, ArticlesRepository $repoArticles, CommandeRepository $repoCommande, UserRepository $repoUser): Response
+    public function historique(CommandeDetailsRepository $repoComD, CommandeRepository $repoCommande): Response
     {
-
         $com = $repoCommande->findAll();
         $art = $repoComD->findAll();
 
