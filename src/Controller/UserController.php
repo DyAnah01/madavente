7f9be8b930c9c590ee8181e7abb0f689f9baa171
Diana Raharinosy <dianaraharinosy@yahoo.com>
2023-06-05 17:01:45 +0200

    modif home payement registration view just a little adjustment

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -23,15 +23,15 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('admin/user/delete/{id}', name: 'delete_user')]
-    public function deleteUser(User $user, EntityManagerInterface $manager)
-    {
-        $idUser = $user->getId();
-        $manager->remove($user);
-        $manager->flush();
-        $this->addFlash("success", "Le user N° $idUser a bien été suppimé");
-        return $this->redirectToRoute("app_user");
-    }
+    // #[Route('admin/user/delete/{id}', name: 'delete_user')]
+    // public function deleteUser(User $user, EntityManagerInterface $manager)
+    // {
+    //     $idUser = $user->getId();
+    //     $manager->remove($user);
+    //     $manager->flush();
+    //     $this->addFlash("success", "Le user N° $idUser a bien été suppimé");
+    //     return $this->redirectToRoute("app_user");
+    // }
 
     #[Route('profile/Information_personnelle', name: 'info_user')]
     public function showInfoUser()
