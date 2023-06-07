121de40b47881201aed31936e86791c1c0dd1084
Diana Raharinosy <dianaraharinosy@yahoo.com>
2023-06-07 12:56:15 +0200

    modif show info user and clean code

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -65,26 +65,6 @@ class ContactType extends AbstractType
                 "id" => "valider"
             ]
         ])
-        // ->add('nom', TextareaType::class, [
-        //     "label"=>"Nom de la catégorie",
-        //     "required"=>false,//mettre valeur par défaut qui est ne nom de propriété à false pour le modifier
-        //     "attr"=>[
-        //         "placeholder"=> "Saisir le nom de la catégorie",
-        //         "class"=>"border border-primary"
-        //     ],
-        //     "constraints"=>[
-        //         new NotBlank([
-        //             "message" => "Veuillez saisir un nom de catégorie"
-
-        //         ]),
-        //         new Length([
-        //             "min" => 5,
-        //             "max"=>10,
-        //             "minMessage" => "5 caractères min 🥲",
-        //             "maxMessage" => "10 caractères max 😅"
-        //         ])
-        //     ]
-        // ])
         ;
     }
 
